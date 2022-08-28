4ad43d99ee1c2e2e42be2ca2c9be5487a72b2e1f
Nathalia <mdc.nathalia@gmail.com>
2022-08-28 13:18:38 +0300

    Add outbound rules to workspace sg

--- services/main.tf
+++ services/main.tf
@@ -1249,6 +1249,14 @@ resource "aws_security_group" "allow_traffic_to_workspace" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
 
 resource "aws_ssm_parameter" "mymlops_workspaces_security_group_id" {
